3f9e50b732db304879abbeb797374dbc79affe73
holistudio <daniel.bin.lu@gmail.com>
2026-05-22 20:27:58 -0400

    Create dumbell.py
    "If there's anything I hate, it's a dumb bell, I hate a dumb bell" - Don Rickles

---
+++ backend/dumbell.py
@@ -0,0 +1,9 @@
+class DummyModel(object):
+    def __init__(self):
+        pass
+
+    def invoke(self, messages):
+        # print(f"\n\nDEBUG:\n{"*"*10}\n{messages}{"*"*10}\n")
+        response = "huh?"
+        return response
+    
