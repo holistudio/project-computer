55dfc8fe4be73cb72064ebc1437d7d9677ab52dc
holistudio <daniel.bin.lu@gmail.com>
2026-05-22 21:15:59 -0400

    init GPT2Agent
    loads the pre-trained GPT-2 weights on init

--- backend/llm/gpt.py
+++ backend/llm/gpt.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 import tensorflow as tf
 
 import tiktoken
-from gpt_download import load_gpt2_params_from_tf_ckpt
+from llm.gpt_download import load_gpt2_params_from_tf_ckpt
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -28,18 +28,6 @@ model_configs = {
 
 CHOOSE_MODEL = "gpt2-small (124M)"
 BASE_CONFIG.update(model_configs[CHOOSE_MODEL])
-
-model_size = CHOOSE_MODEL.split(" ")[-1].lstrip("(").rstrip(")")
-models_dir = "gpt2"
-
-# Define paths
-model_dir = os.path.join(models_dir, model_size)
-
-# Load settings and params
-tf_ckpt_path = tf.train.latest_checkpoint(model_dir)
-
-settings = json.load(open(os.path.join(model_dir, "hparams.json"), "r", encoding="utf-8"))
-params = load_gpt2_params_from_tf_ckpt(tf_ckpt_path, settings)
 
 tokenizer = tiktoken.get_encoding("gpt2")
 
@@ -228,81 +216,102 @@ def token_ids_to_text(token_ids, tokenizer):
     return tokenizer.decode(flat.tolist())
 
 
-def assign(left, right):
-    if left.shape != right.shape:
-        raise ValueError(f"Shape mismatch. Left: {left.shape}, "
-                         "Right: {right.shape}"
-        )
-    return torch.nn.Parameter(torch.tensor(right))
 
-def load_weights_into_gpt(gpt, params):
 
-    gpt.pos_emb.weight = assign(gpt.pos_emb.weight, params['wpe'])
-    gpt.tok_emb.weight = assign(gpt.tok_emb.weight, params['wte'])
 
-    for b in range(len(params["blocks"])): # for each transformer block
 
-        # np.split divides weights into three equal parts for query, key, and value components
-        q_w, k_w, v_w = np.split(
-            (params["blocks"][b]["attn"]["c_attn"])["w"], 3, axis=-1)
+
+class GPT2Agent(object):
+    def __init__(self):
+        # load weights
+        model_size = CHOOSE_MODEL.split(" ")[-1].lstrip("(").rstrip(")")
+        models_dir = "gpt2"
+        model_dir = os.path.join("llm",models_dir, model_size)
+        tf_ckpt_path = tf.train.latest_checkpoint(model_dir)
+        settings = json.load(open(os.path.join(model_dir, "hparams.json"), "r", encoding="utf-8"))
+        params = load_gpt2_params_from_tf_ckpt(tf_ckpt_path, settings)
         
-        gpt.trf_blocks[b].att.W_query.weight = assign(
-            gpt.trf_blocks[b].att.W_query.weight, q_w.T)
-        gpt.trf_blocks[b].att.W_key.weight = assign(
-            gpt.trf_blocks[b].att.W_key.weight, k_w.T)
-        gpt.trf_blocks[b].att.W_value.weight = assign(
-            gpt.trf_blocks[b].att.W_value.weight, v_w.T)
 
-        q_b, k_b, v_b = np.split(
-            (params["blocks"][b]["attn"]["c_attn"])["b"], 3, axis=-1)
-        gpt.trf_blocks[b].att.W_query.bias = assign(
-            gpt.trf_blocks[b].att.W_query.bias, q_b)
-        gpt.trf_blocks[b].att.W_key.bias = assign(
-            gpt.trf_blocks[b].att.W_key.bias, k_b)
-        gpt.trf_blocks[b].att.W_value.bias = assign(
-            gpt.trf_blocks[b].att.W_value.bias, v_b)
-        gpt.trf_blocks[b].att.out_proj.weight = assign(
-            gpt.trf_blocks[b].att.out_proj.weight,
-            params["blocks"][b]["attn"]["c_proj"]["w"].T)
-        gpt.trf_blocks[b].att.out_proj.bias = assign(
-            gpt.trf_blocks[b].att.out_proj.bias,
-            params["blocks"][b]["attn"]["c_proj"]["b"])
-        gpt.trf_blocks[b].ff.layers[0].weight = assign(
-            gpt.trf_blocks[b].ff.layers[0].weight,
-            params["blocks"][b]["mlp"]["c_fc"]["w"].T)
-        gpt.trf_blocks[b].ff.layers[0].bias = assign(
-            gpt.trf_blocks[b].ff.layers[0].bias,
-            params["blocks"][b]["mlp"]["c_fc"]["b"])
-        gpt.trf_blocks[b].ff.layers[2].weight = assign(
-            gpt.trf_blocks[b].ff.layers[2].weight,
-            params["blocks"][b]["mlp"]["c_proj"]["w"].T)
-        gpt.trf_blocks[b].ff.layers[2].bias = assign(
-            gpt.trf_blocks[b].ff.layers[2].bias,
-            params["blocks"][b]["mlp"]["c_proj"]["b"])
-        gpt.trf_blocks[b].norm1.scale = assign(
-            gpt.trf_blocks[b].norm1.scale,
-            params["blocks"][b]["ln_1"]["g"])
-        gpt.trf_blocks[b].norm1.shift = assign(
-            gpt.trf_blocks[b].norm1.shift,
-            params["blocks"][b]["ln_1"]["b"])
-        gpt.trf_blocks[b].norm2.scale = assign(
-            gpt.trf_blocks[b].norm2.scale,
-            params["blocks"][b]["ln_2"]["g"])
-        gpt.trf_blocks[b].norm2.shift = assign(
-            gpt.trf_blocks[b].norm2.shift,
-            params["blocks"][b]["ln_2"]["b"])
-        
-    gpt.final_norm.scale = assign(gpt.final_norm.scale, params["g"])
-    gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
+        self.model = GPTModel(BASE_CONFIG)
+        self._load_weights_into_gpt(self.model, params)
+        print(f"{"*"*10}GPT-2({model_size}) LOADED SUCCESSFULLY{"*"*10}")
+        self.model.eval()
 
-    # weight tying
-    # re-use weights of the token embedding layer now in the output layer
-    gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])
+        pass
 
+    
+    def _load_weights_into_gpt(self, gpt, params):
+        def assign(left, right):
+            if left.shape != right.shape:
+                raise ValueError(f"Shape mismatch. Left: {left.shape}, "
+                                "Right: {right.shape}"
+                )
+            return torch.nn.Parameter(torch.tensor(right))
+        gpt.pos_emb.weight = assign(gpt.pos_emb.weight, params['wpe'])
+        gpt.tok_emb.weight = assign(gpt.tok_emb.weight, params['wte'])
 
-# class GPT2Agent
-# __init__()
-# load weights
+        for b in range(len(params["blocks"])): # for each transformer block
+
+            # np.split divides weights into three equal parts for query, key, and value components
+            q_w, k_w, v_w = np.split(
+                (params["blocks"][b]["attn"]["c_attn"])["w"], 3, axis=-1)
+            
+            gpt.trf_blocks[b].att.W_query.weight = assign(
+                gpt.trf_blocks[b].att.W_query.weight, q_w.T)
+            gpt.trf_blocks[b].att.W_key.weight = assign(
+                gpt.trf_blocks[b].att.W_key.weight, k_w.T)
+            gpt.trf_blocks[b].att.W_value.weight = assign(
+                gpt.trf_blocks[b].att.W_value.weight, v_w.T)
+
+            q_b, k_b, v_b = np.split(
+                (params["blocks"][b]["attn"]["c_attn"])["b"], 3, axis=-1)
+            gpt.trf_blocks[b].att.W_query.bias = assign(
+                gpt.trf_blocks[b].att.W_query.bias, q_b)
+            gpt.trf_blocks[b].att.W_key.bias = assign(
+                gpt.trf_blocks[b].att.W_key.bias, k_b)
+            gpt.trf_blocks[b].att.W_value.bias = assign(
+                gpt.trf_blocks[b].att.W_value.bias, v_b)
+            gpt.trf_blocks[b].att.out_proj.weight = assign(
+                gpt.trf_blocks[b].att.out_proj.weight,
+                params["blocks"][b]["attn"]["c_proj"]["w"].T)
+            gpt.trf_blocks[b].att.out_proj.bias = assign(
+                gpt.trf_blocks[b].att.out_proj.bias,
+                params["blocks"][b]["attn"]["c_proj"]["b"])
+            gpt.trf_blocks[b].ff.layers[0].weight = assign(
+                gpt.trf_blocks[b].ff.layers[0].weight,
+                params["blocks"][b]["mlp"]["c_fc"]["w"].T)
+            gpt.trf_blocks[b].ff.layers[0].bias = assign(
+                gpt.trf_blocks[b].ff.layers[0].bias,
+                params["blocks"][b]["mlp"]["c_fc"]["b"])
+            gpt.trf_blocks[b].ff.layers[2].weight = assign(
+                gpt.trf_blocks[b].ff.layers[2].weight,
+                params["blocks"][b]["mlp"]["c_proj"]["w"].T)
+            gpt.trf_blocks[b].ff.layers[2].bias = assign(
+                gpt.trf_blocks[b].ff.layers[2].bias,
+                params["blocks"][b]["mlp"]["c_proj"]["b"])
+            gpt.trf_blocks[b].norm1.scale = assign(
+                gpt.trf_blocks[b].norm1.scale,
+                params["blocks"][b]["ln_1"]["g"])
+            gpt.trf_blocks[b].norm1.shift = assign(
+                gpt.trf_blocks[b].norm1.shift,
+                params["blocks"][b]["ln_1"]["b"])
+            gpt.trf_blocks[b].norm2.scale = assign(
+                gpt.trf_blocks[b].norm2.scale,
+                params["blocks"][b]["ln_2"]["g"])
+            gpt.trf_blocks[b].norm2.shift = assign(
+                gpt.trf_blocks[b].norm2.shift,
+                params["blocks"][b]["ln_2"]["b"])
+            
+        gpt.final_norm.scale = assign(gpt.final_norm.scale, params["g"])
+        gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
+
+        # weight tying
+        # re-use weights of the token embedding layer now in the output layer
+        gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])
+
+    def invoke(self, messages):
+        response = "huh?"
+        return response
 
 # invoke()
 # convert message to tokens
